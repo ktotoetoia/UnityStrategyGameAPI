d46b3bcf9035bbc2044625085c3f06fa394675f1
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-07-17 17:23:01 -0300

    added pathfinding algorithm

--- Game/Assets/Scripts/CoreLogic/Pathfinding/DijkstraAlgorithm.cs
+++ Game/Assets/Scripts/CoreLogic/Pathfinding/DijkstraAlgorithm.cs
@@ -7,58 +7,64 @@ namespace TDS.Pathfinding
 {
     public class DijkstraAlgorithm
     {
-        public List<INode> GetPath(List<INode> nodes, INode start, INode end, Func<IEdge,float> distances)
+        public List<INode<T>> GetPath<T>(
+            List<INode<T>> nodes,
+            INode<T> start,
+            INode<T> end,
+            Func<IEdge<T>, float> distances)
         {
-            HashSet<INode> unvisited = new HashSet<INode>(nodes);
-            Dictionary<INode, DijkstraData> track = new Dictionary<INode, DijkstraData> { [start] = new DijkstraData { Price = 0 } };
+            HashSet<INode<T>> unvisited = new HashSet<INode<T>>(nodes);
+            Dictionary<INode<T>, DijkstraData<T>> track = new()
+            {
+                [start] = new DijkstraData<T> { Price = 0 }
+            };
 
             while (true)
             {
-                INode current = unvisited
+                INode<T> current = unvisited
                     .Where(n => track.ContainsKey(n))
                     .OrderBy(n => track[n].Price)
                     .FirstOrDefault();
 
-                if (current == null) return new List<INode>();
-                if (current == end) break;
+                if (current == null) return new List<INode<T>>();
+                if (current.Equals(end)) break;
 
                 foreach (var edge in current.Edges)
                 {
-                    INode next = OtherNode(edge,current);
-                    
+                    INode<T> next = OtherNode(edge, current);
+
                     double price = track[current].Price + distances(edge);
 
                     if (!track.ContainsKey(next) || price < track[next].Price)
-                        track[next] = new DijkstraData { Previous = current, Price = price };
+                        track[next] = new DijkstraData<T> { Previous = current, Price = price };
                 }
 
                 unvisited.Remove(current);
             }
-            
+
             return TrackToPath(track, end);
         }
 
-        private INode OtherNode(IEdge edge, INode current)
+        private INode<T> OtherNode<T>(IEdge<T> edge, INode<T> current)
         {
-            return edge.From == current ? edge.To : edge.From;
+            return Equals(edge.From, current) ? edge.To : edge.From;
         }
 
-        private List<INode> TrackToPath(Dictionary<INode, DijkstraData> track, INode end)
+        private List<INode<T>> TrackToPath<T>(Dictionary<INode<T>, DijkstraData<T>> track, INode<T> end)
         {
-            List<INode> path = new List<INode>();
-            
-            for (INode n = end; n != null; n = track[n].Previous)
+            List<INode<T>> path = new();
+            for (var n = end; n != null; n = track[n].Previous)
             {
                 path.Insert(0, n);
             }
-            
+
             return path;
         }
-    }
 
-    class DijkstraData
-    {
-        public INode Previous { get; set; }
-        public double Price { get; set; }
+        class DijkstraData<T>
+        {
+            public INode<T> Previous { get; set; }
+            public double Price { get; set; }
+        }
     }
 }
