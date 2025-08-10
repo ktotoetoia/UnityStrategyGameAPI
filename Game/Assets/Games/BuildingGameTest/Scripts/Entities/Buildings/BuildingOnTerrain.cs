e1db98a4ce8de02551c8016cccf91c44045e84e0
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-08-10 20:48:55 -0300

    feat: add IHaveTerrain for a Building entity

---
+++ Game/Assets/Games/BuildingGameTest/Scripts/Entities/Buildings/BuildingOnTerrain.cs
@@ -0,0 +1,36 @@
+﻿using TDS.Components;
+using TDS.Entities;
+using TDS.Events;
+
+namespace BuildingsTestGame
+{
+    public class BuildingOnTerrain : Component, IHaveTerrain
+    {        
+        private ICallPropertyChange<IGameTerrainComponent> _terrain;
+
+        private ICallPropertyChange<IGameTerrainComponent> _terrainEvent =>
+            _terrain ??= new CallPropertyChange<IGameTerrainComponent, IHaveTerrain>(this,Entity.GetComponent<IEventComponent>());
+
+        public IGameTerrainComponent Terrain
+        {
+            get
+            {
+                ThrowExceptionIfDestroyed();
+                return _terrainEvent.Value;  
+            } 
+            set
+            {
+                ThrowExceptionIfDestroyed();
+                
+                if (_terrainEvent.Value != null)
+                {
+                    _terrainEvent.Value.Building = null;
+                }
+                
+                value.Building = Entity;
+                Entity.Transform.SetPosition(value.Entity.Transform.Position);
+                _terrainEvent.Value = value;
+            }
+        }
+    }
+}
