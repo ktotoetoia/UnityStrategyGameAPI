f06fe311df95f68e56c8fa8eb0d5e78049b006d7
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-08-11 14:59:28 -0300

    feat: add edge extensions

---
+++ Game/Assets/Scripts/CoreLogic/Graph/Edges/EdgeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TDS.Graphs
+{
+    public static class EdgeExtensions
+    {
+        public static INode<T> GetOther<T>(this IEdge<T> edge, INode<T> node)
+        {
+            if(edge.From == node) return edge.To;
+            if(edge.To == node) return edge.From;
+
+            throw new SystemException("edge does not contains this node");
+        }
+    }
+}
