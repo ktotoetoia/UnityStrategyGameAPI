4244b3a057c267bbe971bb3160bfdb9e4505a661
Illia Muzyka <75931710+ktotoetoia@users.noreply.github.com>
2025-08-16 16:19:35 -0300

    feat: add backing property

---
+++ Game/Assets/Games/BuildingGameTest/Scripts/Entities/Buildings/FirstBuilding/BuildingOnTerrain.cs
@@ -0,0 +1,39 @@
+﻿using TDS.Components;
+using TDS.Entities;
+using TDS.Events;
+
+namespace BuildingsTestGame
+{
+    public class BuildingOnTerrain : Component, IPlacedOnTerrain
+    {        
+        [BackingProperty(nameof(PlacedOn))]
+        private ICallPropertyChange<IGameTerrainComponent>_terrain;
+
+        public IGameTerrainComponent PlacedOn
+        {
+            get
+            {
+                ThrowExceptionIfDestroyed();
+                return _terrain.Value;  
+            } 
+            set
+            {
+                ThrowExceptionIfDestroyed();
+                
+                if (_terrain.Value != null)
+                {
+                    _terrain.Value.Building = null;
+                }
+                
+                value.Building = Entity;
+                Entity.Transform.SetPosition(value.Entity.Transform.Position);
+                _terrain.Value = value;
+            }
+        }
+
+        public BuildingOnTerrain()
+        {
+            _terrain = new CallPropertyChange<IGameTerrainComponent>(this);
+        }
+    }
+}
---
+++ Game/Assets/Games/BuildingGameTest/Scripts/Entities/Terrains/GameTerrain/GameTerrainComponent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TDS.Entities;
+using TDS.Events;
+using TDS.Maps;
+
+namespace BuildingsTestGame
+{
+    public class GameTerrainComponent : TerrainComponent, IGameTerrainComponent
+    {
+        [BackingProperty(nameof(Building))] private ICallPropertyChange<IEntity> _building;
+        [BackingProperty(nameof(Unit))] private ICallPropertyChange<IEntity> _unit;
+
+        public IEntity Unit
+        {
+            get => _unit.Value;
+            set
+            {
+                if (value == null)
+                {
+                    _unit.Value = null;
+                
+                    return;
+                }
+                
+                if (_unit.Value != null)
+                {
+                    throw new ArgumentException("can not move unit to the terrain which already have a unit");
+                }
+
+                _unit.Value = value;
+            }
+        }
+
+        public IEntity Building
+        {
+            get => _building.Value;
+            set
+            {
+                if (_building.Value != null && value != null)
+                {
+                    throw new ArgumentException("can not create a building on the terrain which already have a building");
+                }
+                
+                _building.Value = value;
+            }
+        }
+
+        public override void Init(IEntity entity)
+        {
+            base.Init(entity);
+
+            _building = new CallPropertyChange<IEntity>(this);
+            _unit = new CallPropertyChange<IEntity>(this);
+        }
+    }
+}
